ac4b4264d772e89201b835c7942af2702f251ba4
tom orenge <tomorenge3@gmail.com>
2019-02-23 17:05:10 +0300

    added a line break between the form content and the button

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+fuction check(){
+  var questionOne= document.Quiz.questionOne.val()
+  var questionTwo= document.Quiz.questionTwo.val()
+  var questionThree= document.Quiz.questionThree.val()
+  var questionFour= document.Quiz.questionFour.val()
+  var questionFive= document.Quiz.questionFive.val()
+  var questionSix= document.Quiz.questionSix.val()
+  var questionSeven= document.Quiz.questionSeven.val()
+  var questionEight= document.Quiz.questionEight.val()
+  var
+}
