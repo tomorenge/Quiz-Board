a87d13d0757e60ae407784a8a685d40592498b57
tom orenge <tomorenge3@gmail.com>
2019-02-24 09:47:02 +0300

    edited my html structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
-fuction check(){
-  var questionOne= document.Quiz.questionOne.val()
-  var questionTwo= document.Quiz.questionTwo.val()
-  var questionThree= document.Quiz.questionThree.val()
-  var questionFour= document.Quiz.questionFour.val()
-  var questionFive= document.Quiz.questionFive.val()
-  var questionSix= document.Quiz.questionSix.val()
-  var questionSeven= document.Quiz.questionSeven.val()
-  var questionEight= document.Quiz.questionEight.val()
-  var
+var answers= ["var CarName","True","=","while(i<=10)","Both the head section and the body","onclick","myFunction()","alert("Hello World")"];
+var pointPercorrect= 12.5;
+
+function percentage(score) {
+  return "HERE IS YOUR SCORE" + parseInt(score) + "%";
+
 }
+$(document).ready(fuction(){
+  $("#questions").submit(function(event) {
+    $("#results").text("");
+    var score=0;
+    var answerOne= ($("input[type=radio][name=questionOneAnswer]:checked").val());
+    var answerTwo= ($("input[type=radio][name=questionTwoAnswer]:checked").val());
+    var answerThree= ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+    var answerFour= ($("input[type=radio][name=questionFourAnawer]:checked").val());
+    var answerFive= ($("input[type=radio][name=questionFiveAnswer]:checked").val());
+    var answerSix= ($("input[type=radio][name=questionSixAnswer]:checked").val());
+    var answerSeven= ($("input[type=radio][name=questionSevenAnswer]:checked").val());
+    var answerEight= ($("input[type=radio][name=questionEightAnswer]:checked").val());
+
+
+    if(answerOne===12.5%&&answerTwo===12.5%&&answerThree===12.5&&answerFour===12.5&&answerFive===12.5&&answerSix===12.5&&answerSeven===12.5&&answerEight===12.5||answerOne===0) {
+      $("#excellentlyPaassed").text("Excellent ");
+    });
+});
