8b6d432538a40d2252f65607bd19ded3bbbf3716
tom orenge <tomorenge3@gmail.com>
2019-02-25 13:58:29 +0300

    updated business logic in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
-// $(document).ready(function(){
-//   $("button#btn").click(function() {
-//     ;
-// //     event.preventDefault();
-// //
-// //
-// //     var answerOne= ($("input[type=radio][name=questionOne]:checked").val());
-// //     var answerTwo= ($("input[type=radio][name=questionTwo]:checked").val());
-// //     var answerThree= ($("input[type=radio][name=questionThree]:checked").val());
-// //     var answerFour= ($("input[type=radio][name=questionFour]:checked").val());
-// //     var answerFive= ($("input[type=radio][name=questionFive]:checked").val());
-// //     var answerSix= ($("input[type=radio][name=questionSix]:checked").val());
-// //     var answerSeven= ($("input[type=radio][name=questionSeven]:checked").val());
-// //     var answerEight= ($("input[type=radio][name=questionEight]:checked").val());
-// //
-// // var score = (answerOne+answerTwo+answerThree+answerFour+answerFive+answerSix+answerSeven+answerEight);
-// // $("#correctnumber").text("score");
-// };
-// };
+
+
 $(document).ready(function(){
-  $("button#btn").click(function(){
-    alert("rrih");
+  $("button#btn").click(function(event){
+
+
+    event.preventDefault();
+  var answerOne= parseInt($("input[type=radio][name=questionOne]:checked").val());
+  var answerTwo= parseInt($("input[type=radio][name=questionTwo]:checked").val());
+  var answerThree= parseInt($("input[type=radio][name=questionThree]:checked").val());
+  var answerFour= parseInt($("input[type=radio][name=questionFour]:checked").val());
+  var answerFive= parseInt($("input[type=radio][name=questionFive]:checked").val());
+  var answerSix= parseInt($("input[type=radio][name=questionSix]:checked").val());
+  var answerSeven= parseInt($("input[type=radio][name=questionSeven]:checked").val());
+  var answerEight= parseInt($("input[type=radio][name=questionEight]:checked").val());
+  var score = (answerOne+answerTwo+answerThree+answerFour+answerFive+answerSix+answerSeven+answerEight);
+  document.getElementById("result").innerHTML= "Here is your score " + score + "%"
+
   });
 });
